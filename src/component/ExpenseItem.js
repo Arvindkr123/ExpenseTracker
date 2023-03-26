30233e4ee9a6e06fd568e7eeda89da650750b438
Arvind <thakurarvindkr10@gmail.com>
2023-03-27 00:36:08 +0530

    adding the date differently

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,19 +1,26 @@
-import React from 'react'
-import './ExpenseItem.css'
+import React from "react";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US',{month:'long'} );
+    const day = props.date.toLocaleString('en-US',{day:'2-digit'} );
+    const year = props.date.getFullYear();
   return (
-    <div className='expense-item'>
-        <div>{props.date.toUTCString()}</div><br/>
-        <div>
-            <h2>{props.location}</h2>
-        </div>
-        <div className='expense-item__description'>
-            <h2>{props.title}</h2>
-            <div className='expense-item__price'>${props.amount}</div>
-        </div>
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div>
+        <h2>{props.location}</h2>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ExpenseItem;
