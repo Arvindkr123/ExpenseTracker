dfe81c6ceaaddf5cd40b2fb14355d976fde8c90e
Arvind <thakurarvindkr10@gmail.com>
2023-03-30 18:32:46 +0530

    changes expense to 100$ whenever we click on it.Use same logic as change title.

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import ExpenseDate from "../Expenses/ExpenseDate";
 import ExpenseDetails from "../Expenses/ExpenseDetails";
 import "../Expenses/ExpenseItem.css";
 import Card from "../UI/Card";
 
 const  ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
   const clickHandler =()=>{
-    console.log('Clicked!!!!!!!!')
+    setTitle('Updated');
+    console.log(title);
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails
-        amount={props.amount}
+        // amount={props.amount}
+        amount={amount}
         location={props.location}
-        title={props.title}
+        // title={props.title}
+        title={title}
       />
       <button onClick={clickHandler}>change title</button>
-      <button onClick={clickHandler}>Delete Expenses</button>
+      <button onClick={()=>{setAmount(100)}}>change Expenses</button>
     </Card>
   );
 }
