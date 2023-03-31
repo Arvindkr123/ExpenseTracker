2ed97a5c7f87ed47e80fab53ac6cf62b55d807b4
Arvind <thakurarvindkr10@gmail.com>
2023-03-31 08:17:03 +0530

    added other multiple state insted of single state

--- src/component/Expenses/NewExpense/ExpenseForm.js
+++ src/component/Expenses/NewExpense/ExpenseForm.js
@@ -1,23 +1,59 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
-  const [enterTitle, enterSetTitle] = useState(null);
-  const [enterAmount, enterSetAmount] = useState(null);
-  const [enterDate, enterSetDate] = useState(null);
+    // multiple state this initial state
+    const [enterTitle, enterSetTitle] = useState(null);
+    const [enterAmount, enterSetAmount] = useState(null);
+    const [enterDate, enterSetDate] = useState(null);
+
+//   const [UserInput, setUserInput] = useState({
+//     enterTitle: "",
+//     enterAmount: "",
+//     enterDate: "",
+//   });
+
+
   const ChangeTitleHandler = (e) => {
     // console.log(e.target.value);
-    enterSetTitle(e.target.value)
-    console.log(enterTitle)
+    enterSetTitle(e.target.value);
+    console.log(enterTitle);
+
+    // setUserInput({
+    //   ...UserInput,
+    //   enterTitle: e.target.value,
+    // });
+
+    // setUserInput((prevState)=>{
+    //     return {...prevState, enterTitle:e.target.value};
+    // })
   };
   const ChangeAmountHandler = (e) => {
     // console.log(e.target.value);
-    enterSetAmount(e.target.value)
-    console.log(enterAmount)
+    enterSetAmount(e.target.value);
+    console.log(enterAmount);
+
+    // setUserInput({
+    //   ...UserInput,
+    //   enterAmount: e.target.value,
+    // });
+
+    // setUserInput((prevState)=>{
+    //     return {...prevState, enterAmount:e.target.value};
+    // })
   };
   const ChangeDateHandler = (e) => {
     // console.log(e.target.value);
-    enterSetDate(e.target.value)
-    console.log(enterDate)
+    enterSetDate(e.target.value);
+    console.log(enterDate);
+
+    // setUserInput({
+    //   ...UserInput,
+    //   enterDate: e.target.value,
+    // });
+
+    // setUserInput((prevState)=>{
+    //     return {...prevState, enterDate:e.target.value};
+    // })
   };
   return (
     <form>
@@ -28,11 +64,21 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min={0.01} step={0.01} onChange={ChangeAmountHandler} />
+          <input
+            type="number"
+            min={0.01}
+            step={0.01}
+            onChange={ChangeAmountHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min={"2022-01-01"} max={"2023-12-01"} onChange={ChangeDateHandler} />
+          <input
+            type="date"
+            min={"2022-01-01"}
+            max={"2023-12-01"}
+            onChange={ChangeDateHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
