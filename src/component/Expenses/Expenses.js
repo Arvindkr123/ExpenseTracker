1ce3f8fb70bebbb9ac13c6a2e2e4e7d18cba4606
Arvind <thakurarvindkr10@gmail.com>
2023-04-01 17:06:37 +0530

    Adding the key in the Expenses

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -5,17 +5,20 @@ import "../Expenses/Expenses.css";
 import ExpensesFilter from "./NewExpense/ExpenseFilter";
 
 const Expenses = (props) => {
-
-  const [filteredYear, setfilteredYear] = useState('2023');
-  const filterChangeHandler = (selectedYear)=>{
+  const [filteredYear, setfilteredYear] = useState("2023");
+  const filterChangeHandler = (selectedYear) => {
     setfilteredYear(selectedYear);
-  }
-  
+  };
+
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {props.item.map((expense, i) => (
-        <ExpenseItem key={i}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {props.item.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
