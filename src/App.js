9c697a9993e0b39bb7ee252f5e056dbad02d4afe
Arvind <thakurarvindkr10@gmail.com>
2023-04-11 23:38:47 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import Video from "./components/Video";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Video title={'React learning'} color='red' />
+      <Video title={'JS learning'} color='green' />
+      <Video title={'CSS learning'} color='yellow' />
+    </>
+  );
 };
 
 export default App;
